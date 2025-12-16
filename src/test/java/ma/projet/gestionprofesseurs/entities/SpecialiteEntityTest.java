badf2fa1e8e9ebdd4c9dddd38765acffd9ca5fb5
aliraza060f <ali6061f@gmail.com>
2025-12-16 18:30:46 +0500

    Fix SpecialiteEntityTest to match entity fields

--- src/test/java/ma/projet/gestionprofesseurs/entities/SpecialiteEntityTest.java
+++ src/test/java/ma/projet/gestionprofesseurs/entities/SpecialiteEntityTest.java
@@ -1,14 +1,27 @@
 package ma.projet.gestionprofesseurs.entities;
 
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SpecialiteEntityTest {
+class SpecialiteEntityTest {
 
     @Test
-    void testSpecialiteSettersAndGetters() {
+    void testGettersAndSetters() {
         Specialite s = new Specialite();
+
+        s.setCode("INF");
         s.setLibelle("Informatique");
+
+        assertEquals("INF", s.getCode());
         assertEquals("Informatique", s.getLibelle());
+    }
+
+    @Test
+    void testConstructor() {
+        Specialite s = new Specialite("MATH", "Mathematiques");
+
+        assertEquals("MATH", s.getCode());
+        assertEquals("Mathematiques", s.getLibelle());
     }
 }
